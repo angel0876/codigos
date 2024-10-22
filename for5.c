fef880b4b6e0f886a5557bee008e7915f6b7b8da
angel0876 <angelsanchezmartinez40@gmail.com>
2024-10-22 14:48:18 -0600

    commit1

---
+++ for5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int i;
+    int j;
+    int n;
+    
+pust("ingresa un digito");
+scanf("%d",&n);
+
+for(i=0; i,n; i++){
+   //printf("*");
+   for(j=0; j<=i; j++);{
+printf("*");
+   }
+
+   printf("\n");
+
+   for(i=0; i<n; i++){
+   //printf("*\t\t");
+   for(j=0; j<=i; j++);{
+printf("*");
+   }  
+}
+
+return 0;
+}
+}
