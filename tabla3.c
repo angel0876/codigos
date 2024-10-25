8c55d4d588a3738a09cb1b4776f50e3778d86ae4
angel0876 <angelsanchezmartinez40@gmail.com>
2024-10-25 12:06:10 -0600

    ejercicio 2 c

---
+++ tabla3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int miArreglo[100];
+    int i=99;
+    
+    while (i>=0)
+    {
+        printf("miArreglo [%d]\n", i);
+        i++;
+
+    }
+     
+        
+    }
+
