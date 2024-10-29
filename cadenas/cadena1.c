7d1b0daed88e11e9dc0fda81a12bbd30bfabb459
angel0876 <angelsanchezmartinez40@gmail.com>
2024-10-29 13:03:38 -0600

    ejercicio cadenas

---
+++ cadenas/cadena1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char cad[10];
+    printf("introduce una palbra\n");
+    scanf("%s",cad);
+    printf("\n %s", cad);
+    return 0;
+    //strlen devuelve la longitud de la cadena din tomar en cuenta el caracter de final de cadena
+    
+}
