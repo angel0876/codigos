ea45a8cb03e81925193691001ddad7b9f3092452
angel0876 <angelsanchezmartinez40@gmail.com>
2024-11-20 17:56:50 -0600

    cadenas

---
+++ cadenas/cadena3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int i;
+    int len;
+    char cad[]="";
+    printf("");
+
+//strcpy copia contenido de <cadena_origen> en <cadena_destino>
+
+
+}
