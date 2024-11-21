aa3d171d6f1926fa4de724345cf74c6c64aeaa33
angel0876 <angelsanchezmartinez40@gmail.com>
2024-11-21 11:32:36 -0600

    cadenas

---
+++ cadenas/cadena4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int lend;
+    char origen[]="origen";
+    char destino[7];
+    strcpy(destino,origen);
+    printf("destino : %s",destino);
+    system("pause");
+    return 0 ;
+    
+}
