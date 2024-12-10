4033e0a6cfdf422bad713beeed43c5d76dd88fb5
angel0876 <angelsanchezmartinez40@gmail.com>
2024-12-10 15:01:04 -0600

    apuntadores.c

---
+++ cadenas/fonciones recursivas/estructuras/Apuntadores/apuntador1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int  main(){ 
+    int a=20;
+    printf("hola el valor de a es:%d \n,a");
+    printf("la direccion de memoria de a es:%p",&a);
+    return 0;
+
+}
