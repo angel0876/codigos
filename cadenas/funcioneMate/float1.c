376568726829d565b384c0111df3c5f511be589f
angel0876 <angelsanchezmartinez40@gmail.com>
2024-12-16 17:59:11 -0600

    ejercicios.c

---
+++ cadenas/funcioneMate/float1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+float promedio(float a, float b);
+int main(){
+    float a=5,b=10,prom;
+    prom=promedio(a,b);
+    printf("el promedio es:%2.1f/n",prom);
+    return 0 ;
+
+}
